9318d993cf06230c56b44b79936b35534828e26e
Davi Dutra <davidutramn@gmail.com>
2021-10-17 00:51:15 -0300

    Exercicio para exibir as informaçoes do os

---
+++ aulas/basico/osinfo/osinfo.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	fmt.Println("OS: \t\t", runtime.GOOS)
+	fmt.Println("ARCH: \t\t", runtime.GOARCH)
+	fmt.Println("CPUs: \t\t", runtime.NumCPU())
+	fmt.Println("Go Version: \t", runtime.Version())
+}
