ad6509c2b59ece29065e9de1045522eadadaddbe
Davi Dutra <davidutramn@gmail.com>
2021-11-03 14:44:40 -0300

    Exibindo as informaçoes de forma organizada

--- miniprojetos/githubuser/main.go
+++ miniprojetos/githubuser/main.go
@@ -20,5 +20,29 @@ func main() {
 		log.Panic(err)
 	}
 
-	fmt.Printf("%#v", user)
+	printUser(user)
+}
+
+func printUser(user *GitHubUser) {
+	fmt.Println("Login", user.Login)
+	fmt.Println("ID", user.ID)
+	fmt.Println("NodeID", user.NodeID)
+	fmt.Println("AvatarURL", user.AvatarURL)
+	fmt.Println("GravatarID", user.GravatarID)
+	fmt.Println("Type", user.Type)
+	fmt.Println("SiteAdmin", user.SiteAdmin)
+	fmt.Println("Name", user.Name)
+	fmt.Println("Company", user.Company)
+	fmt.Println("Blog", user.Blog)
+	fmt.Println("Location", user.Location)
+	fmt.Println("Email", user.Email)
+	fmt.Println("Hireable", user.Hireable)
+	fmt.Println("Bio", user.Bio)
+	fmt.Println("TwitterUsername", user.TwitterUsername)
+	fmt.Println("PublicRepos", user.PublicRepos)
+	fmt.Println("PublicGists", user.PublicGists)
+	fmt.Println("Followers", user.Followers)
+	fmt.Println("Following", user.Following)
+	fmt.Println("CreatedAt", user.CreatedAt)
+	fmt.Println("UpdatedAt", user.UpdatedAt)
 }
